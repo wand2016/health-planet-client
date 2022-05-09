e3d47e81a7cb873f7f55affb803424113ba6c279
wand2016 <d19921207@gmail.com>
2022-05-09 19:56:33 +0900

    雑にログアウトメニュー追加

---
+++ README.md
@@ -0,0 +1,3 @@
+npm install @mui/material @emotion/react @emotion/styled
+
+yarn add @mui/material @emotion/react @emotion/styled
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,67 @@
+import React from "react";
 import "./App.css";
-import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import {
+  AppBar,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { AccountCircle } from "@mui/icons-material";
 
 function App() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar position="static">
       <Toolbar variant="dense">
-        <IconButton
-          edge="start"
+        <Typography
+          variant="h6"
           color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
+          component="div"
+          sx={{ flexGrow: 1 }}
         >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" color="inherit" component="div">
-          Photos
+          Health Planet Client
         </Typography>
+        <div>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenu}
+            color="inherit"
+          >
+            <AccountCircle />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>OAuth2.0 Settings</MenuItem>
+            <MenuItem onClick={handleClose}>Logout</MenuItem>
+          </Menu>
+        </div>
       </Toolbar>
     </AppBar>
   );
