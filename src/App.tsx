9a376fb3bb88e5d334438c738fe19ca9c4c51df6
wand2016 <d19921207@gmail.com>
2022-05-09 20:38:35 +0900

    axios/useSWR導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 import AppBar from "@/layouts/AppBar";
+import useSWR, { Fetcher } from "swr";
+import axiosInstance from "@/api";
+
+const fetcher: Fetcher = async (url: string) => {
+  const response = await axiosInstance.get(url);
+
+  return response.data;
+};
 
 function App() {
+  const { data, error } = useSWR("/status/innerscan.json", fetcher);
+
+  if (error) {
+    <div>error</div>;
+  }
+
+  if (!data) {
+    <div>...loading...</div>;
+  }
+
   return (
     <div>
       <AppBar />
-      aaa
+      {JSON.stringify(data, null, 2)}
     </div>
   );
 }
