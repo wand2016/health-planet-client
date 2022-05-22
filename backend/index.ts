975f19ecd43846b7a00283797a7898ff15e74599
wand2016 <d19921207@gmail.com>
2022-05-22 21:29:37 +0900

    tokenエンドポイント作成

--- backend/index.ts
+++ backend/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import { AuthorizationCode } from "simple-oauth2";
 
 const app = express();
+app.use(express.json());
 
 app.get("/auth", (req, res) => {
   const client = new AuthorizationCode({
@@ -21,6 +22,30 @@ app.get("/auth", (req, res) => {
   });
 
   res.redirect(uri);
+});
+
+type TokenRequestBody = {
+  code: string;
+};
+
+app.post<any, any, any, TokenRequestBody>("/token", async (req, res) => {
+  const client = new AuthorizationCode({
+    client: {
+      id: process.env.CLIENT_ID,
+      secret: process.env.CLIENT_SECRET,
+    },
+    auth: {
+      tokenHost: process.env.OAUTH_HOST,
+    },
+  });
+
+  const accessToken = await client.getToken({
+    code: req.body.code, // from request
+    redirect_uri: process.env.REDIRECT_URI,
+    scope: "innerscan",
+  });
+
+  res.json(accessToken);
 });
 
 app.listen(3001);
